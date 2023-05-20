ca031be6fc74dab4b03d9aa8741b2ab4c81eb9c1
Jory Roberts <robertsjory13@gmail.com>
2023-05-20 03:49:51 -0500

    displays My Projects

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,26 @@
-import React from "react";
-import ProjectItem from "./ProjectItem";
+import React from 'react';
+import ProjectItem from './ProjectItem';
 
 function ProjectList({ projects }) {
-  console.log(projects);
-  return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
-    </div>
-  );
+    console.log(projects);
+
+    const allProjects = projects.map((project) => (
+        <ProjectItem
+            key={project.id}
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+        ></ProjectItem>
+    ));
+
+    console.log(allProjects);
+
+    return (
+        <div id='projects'>
+            <h2>My Projects</h2>
+            <div id='project-list'>{allProjects}</div>
+        </div>
+    );
 }
 
 export default ProjectList;
